0318bf8394178d417c690579e6fb1df1c4776cc3
Masiha mhmdpour <masihmooohmmadpoor@gmail.com>
2022-09-23 12:17:05 +0330

    json-server added

--- src/components/Write/Write.jsx
+++ src/components/Write/Write.jsx
@@ -7,14 +7,14 @@ const Write = () => {
         <div>
             <div></div>
             <Form>
-                <div  className="m-5">
-                <Form.Label className={styles.inputLabel}>Title :</Form.Label>
-                <Form.Control type="text" className={"w-75"} placeholder='Title...'/>
-                <p className="w-75 d-flex justify-content-end">2/40</p>
-                <Form.Label className={styles.inputLabel}>Text :</Form.Label>
-                <Form.Control placeholder="Text..." as="textarea" className={`${styles.textarea} mb-5`}></Form.Control>
-                <Button styles={{fontFamily : "comfortaa"}} className="mx-3">Add note</Button>
-                <Button styles={{fontFamily : "comfortaa"}} variant='danger' className="writeBtn">Cancel</Button>
+                <div className="m-5">
+                    <Form.Label className={styles.inputLabel}>Title :</Form.Label>
+                    <Form.Control type="text" className={"w-75"} placeholder='Title...' />
+                    <p className="w-75 d-flex justify-content-end">2/40</p>
+                    <Form.Label className={styles.inputLabel}>Text :</Form.Label>
+                    <Form.Control placeholder="Text..." as="textarea" rows={10} className={`${styles.textarea} mb-5`}></Form.Control>
+                    <Button styles={{ fontFamily: "comfortaa" }} className="mx-3">Add note</Button>
+                    <Button styles={{ fontFamily: "comfortaa" }} variant='danger' className="writeBtn">Cancel</Button>
                 </div>
             </Form>
         </div>
