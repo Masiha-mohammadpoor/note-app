63d82f0da476955b6b9a91b7df15e69b73fc5cec
Masiha mhmdpour <masihmooohmmadpoor@gmail.com>
2022-09-28 23:07:19 +0330

    created privateList and handlers

--- src/components/PrivateNotes/PrivateNotes.jsx
+++ src/components/PrivateNotes/PrivateNotes.jsx
@@ -1,15 +1,39 @@
 import styles from "./privateNotes.module.scss";
 import lockImg from "../../assets/image/lockImg.svg";
 import Button from "react-bootstrap/Button";
+import Note from "../Note/Note";
+import { InfinitySpin } from 'react-loader-spinner';
+import {Link} from "react-router-dom";
+import nothingImg from "../../assets/image/nothingImg.svg";
 import swal from "sweetalert";
-import {useState} from "react";
+import {useState , useEffect} from "react";
+import getAllData from "../../Services/getAllData";
+import editData from "../../Services/editData";
+import deleteData from "../../Services/deleteData";
 
 
 const PrivateNotes = () => {
 
+    const [notes , setNotes] = useState([]);
+    const [request , setRequest] = useState("");
     const [password , setPassword] = useState("");
     const [lock , setLock] = useState(false);
     const pass = JSON.parse(localStorage.getItem("password"));
+
+    useEffect(() => {
+        const getData = async () => {
+            try{
+                const {data , statusText} = await getAllData();
+                const filtered = await data.filter(n => n.type === "private");
+                setRequest(statusText)
+                setNotes(filtered);
+            }catch(err){
+                console.log(err);
+            }
+        }
+        getData()
+    } , []);
+
 
     const passwordChangeHandler = (e) => {
         const value = e.target.value;
@@ -40,27 +64,82 @@ const PrivateNotes = () => {
                     {!pass ? "please enter a password" : "please enter your password"}
                 </h5>
                 <input 
-                type="password" 
-                className={styles.passwordField} 
-                autoFocus
-                value={password}
-                onChange={passwordChangeHandler}
-                pattern="[0-9]" />
+                    type="password" 
+                    className={styles.passwordField} 
+                    autoFocus
+                    value={password}
+                    onChange={passwordChangeHandler}
+                    pattern="[0-9]" />
                 <p className="mb-4">5/10</p>
                 <Button onClick={submitHandler} style={{backgroundColor:"#5b3196" , width : "100px" , fontFamily : "comfortaa"}} className="border-0">click</Button>
             </article>
         )
     }
 
-    const privateNoteList = () => {
-        return (
-            <p>masiha mohammadpour</p>
-        )
+    const nothingNotes = () => {
+        return (    
+            <article className={`${styles.nothingContainer} d-flex justify-content-evenly align-items-center flex-column`}>
+                <img src={nothingImg} alt="nothing"/>
+                <h5 className="text-secondary">Nothing is available</h5>
+                <Link to="/write"><Button style={{backgroundColor:"#4c366b"}} className="border-0">add note ?</Button></Link>
+            </article>
+        );
     }
+
+    const deleteHandler = async (id) => {
+        try{
+            const filtered = notes.filter(n => n.id !== id);
+            setNotes(filtered);
+            await deleteData(id);
+        }catch(err){
+            console.log(err)
+        }
+    }
+
+
+    const likeHandler = async (noteData) => {
+        try{
+            const {title , text , type , like , id} = noteData;
+            const index = notes.findIndex(n => n.id === id);
+            const note = {...notes[index]};
+            note.like = !like;
+            const allNotes = [...notes];
+            allNotes[index] = note;
+            setNotes(allNotes);
+            await editData(id , {title , text , type , like : !like});
+        }catch(err){
+            console.log(err);
+        }
+    }   
+
+
+    const renderNotes = () => {
+        if (notes.length >= 1 ){
+            return notes.map(n => {
+                return <Note
+                key={n.id}
+                id={n.id}
+                title={n.title}
+                text={n.text}
+                type={n.type}
+                like={n.like}
+                onLike={() => likeHandler({...n})}
+                onDelete={() => deleteHandler(n.id)} /> 
+            })
+        }else if(notes.length === 0 && request !== ""){
+            return nothingNotes()
+        }else{
+            return <div className={styles.loaderContainer}><InfinitySpin 
+                width='200'
+                color="#8562b5"
+              /></div>
+        }
+    }
+
 
     return ( 
         <>
-            <section className={`m-5`}>{lock ? privateNoteList() : lockComponent()}</section>
+            <section className={`${lock && notes.length >= 1 ? styles.noteList : styles.loaderContainer} mt-4 mb-5`}>{lock ? renderNotes() : lockComponent()}</section>
         </>
      );
 }
